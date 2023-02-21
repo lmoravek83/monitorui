3b3a2c9d7f2978d4aebe6131277fde507b13853b
Lukas Moravek <lmoravek@gmail.com>
2023-02-21 01:09:56 +0100

    Windows install fine tunning - requirements

--- install/install_windows.bat
+++ install/install_windows.bat
@@ -1,6 +1,5 @@
+python -m pip install -U pip
+pip install -r requirements_win.txt
 cd ..
 rename sites-example sites
 rename config-example config
-
-python -m pip install -U pip
-pip install -r requirements_win.txt
